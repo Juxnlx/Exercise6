bdefd547b3e04b578016a1a05ec5bb2c0107add0
jlbarrionuevo <jlbarrionuevo@106-02.info2>
2024-09-20 14:05:12 +0200

    Exercise 6 - Terminado

---
+++ Exercise6/src/exercise6/Exercise6.java
@@ -0,0 +1,14 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Nombre --> Juan Luis Barrionuevo Risquez");
+		System.out.println("Dirección --> Avenida Alcalde Juan Fernandez Nº7 4 izq");
+		System.out.println();
+		System.out.println("Teléfono --> 644477061");
+
+	}
+
+}
