f21457efc2db45b2ad022852f0c2f2e96bbc2c48
juanl2004 <juanluis.barrionuevo@iesnervion.es>
2024-09-21 02:09:35 +0200

    Exercise 6 - Corregido desde casa

--- Exercise6/src/exercise6/Exercise6.java
+++ Exercise6/src/exercise6/Exercise6.java
@@ -7,7 +7,7 @@ public class Exercise6 {
 		System.out.println("Nombre --> Juan Luis Barrionuevo Risquez");
 		System.out.println("Dirección --> Avenida Alcalde Juan Fernandez Nº7 4 izq");
 		System.out.println();
-		System.out.println("Teléfono --> 644477061");
+		System.out.println("Teléfono --> 123456789");
 
 	}
 
